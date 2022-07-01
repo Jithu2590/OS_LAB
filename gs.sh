ecd348e80bfb795807e8a0333c4e4cca758dc217
Vennapusa Jithendra Reddy <106762673+Jithu2590@users.noreply.github.com>
2022-07-01 09:02:47 +0530

    Update gs.sh

--- gs.sh
+++ gs.sh
@@ -17,3 +17,17 @@ else
         gsal=$(echo "$hra+$da+$sal"|bc)
         echo "The gross salary : $gsal"
 fi
+
+------------------------------------------------------------------------------------
+echo "enter the basic salary:"
+read bsal
+if [ $bsal -lt 1500 ]
+then
+gsal=$((bsal+((bsal/100)*10)+(bsal/100)*90))
+echo "The gross salary : $gsal"
+fi
+if [ $bsal -ge 1500 ]
+then
+gsal=$(((bsal+500)+(bsal/100)*98))
+echo "the gross salary : $gsal"
+fi
